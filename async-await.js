da1802340df164d7d1beeb2586e1e9368da8dbb1
Will Walton <will@endless3dge.com>
2024-05-10 17:42:52 -0500

    Update async-await.js
    moved the try catch block because of top-level

--- async-await.js
+++ async-await.js
@@ -2,22 +2,37 @@
 import fs from 'node:fs/promises';
 
 async function loadData() {
-  try {
-    const data = await fs.readFile('./data.json', 'utf8'); // the promise will need to complete and then it will execute the next steps that are listed in the code
-    console.log('File Read 1');
-    await fs.readFile('./data.json', 'utf8');
-    console.log('File Read 2');
-    await fs.readFile('./data.json', 'utf8');
-    console.log('File Read 3');
-		const dataObj = JSON.parse(data);
-		console.log(dataObj);
-		console.log('Complete');
-  } catch (error) {
-    console.log("Could not load and parse file");
-    throw err;
+  // try {
+  //   const data = await fs.readFile('./data.json', 'utf8'); // the promise will need to complete and then it will execute the next steps that are listed in the code
+  //   console.log('File Read 1');
+  //   await fs.readFile('./data.json', 'utf8');
+  //   console.log('File Read 2');
+  //   await fs.readFile('./data.json', 'utf8');
+  //   console.log('File Read 3');
+	// 	const dataObj = JSON.parse(data);
+	// 	console.log(dataObj);
+	// 	console.log('Complete');
+  // } catch (error) {
+  //   console.log("Could not load and parse file");
+  //   throw err;
 
-  }
+  // }
 
 }
 
 loadData().then(() => console.log("Promise completed"));
+
+  try {
+		const data = await fs.readFile('./data.json', 'utf8'); // the promise will need to complete and then it will execute the next steps that are listed in the code
+		console.log('File Read 1');
+		await fs.readFile('./data.json', 'utf8');
+		console.log('File Read 2');
+		await fs.readFile('./data.json', 'utf8');
+		console.log('File Read 3');
+		const dataObj = JSON.parse(data);
+		console.log(dataObj);
+		console.log('Complete');
+	} catch (error) {
+		console.log('Could not load and parse file');
+		throw err;
+	}
