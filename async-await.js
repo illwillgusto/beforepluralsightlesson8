59918ef649e6013533bf584ef0c62baa65a6e52e
Will Walton <will@endless3dge.com>
2024-05-10 17:35:54 -0500

    Update async-await.js
    try catch block added

--- async-await.js
+++ async-await.js
@@ -2,10 +2,17 @@
 import fs from 'node:fs/promises';
 
 async function loadData() {
-	const data = await fs.readFile('./data.json', 'utf8'); // the promise will need to complete and then it will execute the next steps that are listed in the code
-	const dataObj = JSON.parse(data);
-	console.log(dataObj);
-	console.log('Complete');
+  try {
+    const data = await fs.readFile('./data.json', 'utf8'); // the promise will need to complete and then it will execute the next steps that are listed in the code
+		const dataObj = JSON.parse(data);
+		console.log(dataObj);
+		console.log('Complete');
+  } catch (error) {
+    console.log("Could not load and parse file");
+    throw err;
+
+  }
+
 }
 
 loadData().then(() => console.log("Promise completed"));
