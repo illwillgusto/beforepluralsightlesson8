b2086fa506b516a85945372efd65ce58217b1f8e
Will Walton <will@endless3dge.com>
2024-05-10 17:34:11 -0500

    Update async-await.js
    call the loadData and a final log statement

--- async-await.js
+++ async-await.js
@@ -1,9 +1,11 @@
 // Import Node Filesystem Module (the promises version)
-import fs from "node:fs/promises";
+import fs from 'node:fs/promises';
 
 async function loadData() {
-  const data = await fs.readFile('./data.json', 'utf8'); // the promise will need to complete and then it will execute the next steps that are listed in the code
-  const dataObj = JSON.parse(data);
-    console.log(dataObj);
-    console.log("Complete");
+	const data = await fs.readFile('./data.json', 'utf8'); // the promise will need to complete and then it will execute the next steps that are listed in the code
+	const dataObj = JSON.parse(data);
+	console.log(dataObj);
+	console.log('Complete');
 }
+
+loadData().then(() => console.log("Promise completed"));
