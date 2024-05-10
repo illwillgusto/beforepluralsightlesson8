721d9c16748b35c42580a17f4384df0dc41b6185
Will Walton <will@endless3dge.com>
2024-05-10 17:31:49 -0500

    Update async-await.js
    comment explaining what the await code will be doing behind the scenes

--- async-await.js
+++ async-await.js
@@ -2,5 +2,6 @@
 import fs from "node:fs/promises";
 
 async function loadData() {
-  const data = await fs.readFile('./data.json', 'utf8');
+  const data = await fs.readFile('./data.json', 'utf8'); // the promise will need to complete and then it will execute the next steps that are listed in the code
+  
 }
