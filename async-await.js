82f1199bd3f89f5d627f3524af0d103d2bba53e8
Will Walton <will@endless3dge.com>
2024-05-10 17:38:47 -0500

    Update async-await.js
    added await code to read the file 3 more times and log each time the file is read

--- async-await.js
+++ async-await.js
@@ -4,6 +4,11 @@ import fs from 'node:fs/promises';
 async function loadData() {
   try {
     const data = await fs.readFile('./data.json', 'utf8'); // the promise will need to complete and then it will execute the next steps that are listed in the code
+    console.log('File Read 1');
+    await fs.readFile('./data.json', 'utf8');
+    console.log('File Read 2');
+    await fs.readFile('./data.json', 'utf8');
+    console.log('File Read 3');
 		const dataObj = JSON.parse(data);
 		console.log(dataObj);
 		console.log('Complete');
