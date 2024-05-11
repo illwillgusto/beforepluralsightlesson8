b137f40014c33d35ffcbc4339db12853a331bc76
Will Walton <will@endless3dge.com>
2024-05-11 14:40:25 -0500

    handling the currency data
    removed how we handle the currency data and the get Salary data
    
    created a new currency.js file

--- index.js
+++ index.js
@@ -11,33 +11,6 @@ import chalk from 'chalk';
 let employees = [];
 let currencyData;
 
-// Currency data ---------------------------
-const getCurrencyConversionData = async () => {
-  const myHeaders = new Headers();
-	myHeaders.append('apikey', '0c61820d59c22e2607a9cf4cd41187e3');
-  const options = {
-		method: 'GET',
-		headers: myHeaders,
-		redirect: 'follow',
-	};
-  const response = await fetch(
-		'http://api.exchangeratesapi.io/v1/latest?access_key=0c61820d59c22e2607a9cf4cd41187e3&symbols=USD,AUD,CAD,PLN,MXN&format=1',
-		options
-	);
-  if(!response.ok) {
-    throw new Error("Cannot fetch currency data.");
-  }
-  currencyData = await response.json();
-}
-
-const getSalary = (amountUSD, currency) => {
-  const amount = (currency === "USD") ? amountUSD : amountUSD * currencyData.rates[currency];
-  const formatter = Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: currency
-  });
-  return formatter.format(amount);
-}
 
 
 
